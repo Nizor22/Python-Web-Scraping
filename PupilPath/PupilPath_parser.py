2374c4bae56b4c74f49ee95e4df6c68fd3469fde
Nizor22 <meganizor22@gmail.com>
2020-07-15 15:11:07 -0400

    created a PupilPath parser

---
+++ PupilPath/PupilPath_parser.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# Parses the grades from PupilPath.html and adds them to the grades list.
+def parser(html_doc, grades):
+    with open(html_doc) as html_file:
+        soup = BeautifulSoup(html_file, 'html.parser')
+        table = soup.find('tbody').find_all('span')
+        for grade in table:
+            grades.append(float(grade.contents[1].strip()))
+
+
+def main():
+    grades = []
+    parser('PupilPath.html', grades)
+    print('*_*_*_*_*_PUPILPATH GRADES_*_*_*_*_*\n')
+    for grade in grades:
+        if grade >= 91:
+            print('===============HONORS===============')
+            print(grade)
+        else:
+            print('===============NOT_HONORS===========')
+            print(grade)
+
+
+if __name__ == "__main__":
+    main()
--- parsingTutorial/parsingTutorial.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-# Parses the article from the simple.html.
-with open('simple.html') as html_file:
-	soup = BeautifulSoup(html_file, 'lxml')
-article = soup.find('div', class_='article')
-print(article)
