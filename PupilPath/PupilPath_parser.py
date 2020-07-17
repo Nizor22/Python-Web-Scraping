da15c5cade9de582459b6cea67c0ba0073cd9d0a
Nizor22 <farukhzoda.n@gmail.com>
2020-07-16 21:21:32 -0400

    pycharm files

--- PupilPath/PupilPath_parser.py
+++ PupilPath/PupilPath_parser.py
@@ -1,6 +1,9 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
+def login:
+    
 
 # Parses the grades from PupilPath.html and adds them to the grades list.
 def parser(html_doc, grades):
