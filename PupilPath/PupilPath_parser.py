62fb354c57c98fb13eab798f8614e5b8071abb42
Nizor22 <farukhzoda.n@gmail.com>
2020-07-16 22:32:02 -0400

    added comments

--- PupilPath/PupilPath_parser.py
+++ PupilPath/PupilPath_parser.py
@@ -1,23 +1,21 @@
-import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-import time
+from tkinter import *
 
 
+# Logs into PupilPath and saves the page PupilPath.html
 def login(user_name, password):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://pupilpath.skedula.com/')
     driver.find_element_by_id('sign_in').click()
     driver.find_element_by_id('user_username').send_keys(user_name)
     driver.find_element_by_id('sign_in').click()
-    # time.sleep(0.5)
     driver.find_element_by_id('user_password').send_keys(password)
     driver.find_element_by_id('sign_in').click()
     driver.find_element_by_class_name('ui-button-text').click()
     with open('PupilPath.html', 'w') as file:
         file.write(driver.page_source)
-    time.sleep(4)
 
 
 # Parses the grades from PupilPath.html and adds them to the grades list.
@@ -36,6 +34,7 @@ def main():
     login(user_name, password)
     parser('PupilPath.html', grades)
     print('*_*_*_*_*_PUPILPATH GRADES_*_*_*_*_*\n')
+    # Lists all the grades in the grades list.
     for grade in grades:
         if grade >= 91:
             print('===============HONORS===============')
